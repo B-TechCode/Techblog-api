c93e1decde0619d484e9bb4e8660a4b2df9a7a80
B-TechCode <aakashchaurasiya630@gmail.com>
2026-04-05 10:17:28 +0530

    Initial commit: Full-stack TechBlog (Spring Boot + React)

--- src/main/java/com/techblog/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.techblog.repository;
-
-import com.techblog.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Integer> {
-}
-
-
